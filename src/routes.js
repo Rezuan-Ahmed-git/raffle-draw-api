a03d31ea04e68d4f9a11ec81b31f02179a905714
Rezuan Ahmed <rezuan.ahmed.pro@gmail.com>
2023-05-14 14:09:08 +0600

    feat: get tickets controllers implement

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,23 @@
 const router = require('express').Router();
-const { sellBulkTicket, sellSingleTicket } = require('./controllers');
+const {
+  sellBulkTicket,
+  sellSingleTicket,
+  findAll,
+  findById,
+  findByUsername,
+} = require('./controllers');
 
 // router.get('/t/:id');
 // router.put('/t/:id');
 // router.delete('/t/:id');
 
-router.route('/t/:id').get().put().delete();
+router.route('/t/:id').get(findById).put().delete();
 
 // router.get('/u/:username');
 // router.put('/u/:username');
 // router.delete('/u/:username');
 
-router.route('/u/:username').get().put().delete();
+router.route('/u/:username').get(findByUsername).put().delete();
 
 router.post('/bulk', sellBulkTicket);
 router.get('/draw');
@@ -19,6 +25,6 @@ router.get('/draw');
 // router.post('/');
 // router.get('/');
 
-router.route('/').get().post(sellSingleTicket);
+router.route('/').get(findAll).post(sellSingleTicket);
 
 module.exports = router;
